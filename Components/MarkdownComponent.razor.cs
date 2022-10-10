f94f012f0f40c32482686ebaa3f20e211fa88798
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2022-10-10 14:35:51 -0400

    Test 2

---
+++ Components/MarkdownComponent.razor.cs
@@ -0,0 +1,27 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Components;
+
+namespace Components;
+
+public partial class MarkdownComponent
+{
+    private string? _content;
+
+    [Parameter] public string FilePath { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _content = await LoadMarkdown(HttpClient, FilePath);
+    }
+
+    private static readonly MarkdownPipeline Pipeline
+        = new MarkdownPipelineBuilder()
+            .UseYamlFrontMatter()
+            .UseAdvancedExtensions()
+            .Build();
+
+    private static async Task<string> LoadMarkdown(HttpClient httpClient, string filepath)
+    {
+        return await httpClient.GetStringAsync(filepath);
+    }
+}
